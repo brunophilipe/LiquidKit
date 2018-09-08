80974ba85bede5211e7b7f373777656b3bfa1cba
Bruno Philipe <git@bruno.ph>
2018-09-08 22:14:32 +0200

    Add Xcode project file

---
+++ Sources/Liquid/Liquid.h
@@ -0,0 +1,18 @@
+//
+//  Liquid.h
+//  Liquid
+//
+//  Created by Bruno Philipe on 8/9/18.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Liquid.
+FOUNDATION_EXPORT double LiquidVersionNumber;
+
+//! Project version string for Liquid.
+FOUNDATION_EXPORT const unsigned char LiquidVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Liquid/PublicHeader.h>
+
+
