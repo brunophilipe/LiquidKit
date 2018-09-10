7b141362377491825a4ddeb44561580f3dd7fdb5
Bruno Philipe <git@bruno.ph>
2018-09-10 22:39:37 +0200

    Add Double truncation extension method

---
+++ Sources/Liquid/Double+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  Double+Extensions.swift
+//  LiquidTests
+//
+//  Created by Bruno Philipe on 10/10/18.
+//
+
+extension Double
+{
+	/// Given a number of significant digits `count`, returns a version of the receiver with that amount of decimal
+	/// digits set.
+	///
+	/// - Parameter count: Number of significant decimal difgits to truncate the receiver to.
+	/// - Returns: The truncated value
+	func truncatingDecimals(to count: Int) -> Double
+	{
+		guard count >= 0 else
+		{
+			return self
+		}
+
+		return Double(String(format: "%.\(count)f", self)) ?? self
+	}
+}
