9a907dce1e06f27e975d052dd502a8236855bba4
Bruno Philipe <git@bruno.ph>
2018-09-11 22:31:26 +0200

    Re-indents Parser file

--- Sources/Liquid/Parser.swift
+++ Sources/Liquid/Parser.swift
@@ -9,13 +9,14 @@
 import Foundation
 
 /// A class for parsing an array of tokens and converts them into a collection of Node's
-open class TokenParser {
-    
+open class TokenParser
+{
     private var tokens: [Token]
     private let context: Context
 	private var filters: [Filter] = []
     
-    public init(tokens: [Token], context: Context) {
+    public init(tokens: [Token], context: Context)
+	{
         self.tokens = tokens
         self.context = context
 
@@ -34,17 +35,21 @@ open class TokenParser {
 	}
     
     /// Parse the given tokens into nodes
-    public func parse() -> [String] {
+    public func parse() -> [String]
+	{
         return parse(nil)
     }
     
-    public func parse(_ parse_until:((_ parser:TokenParser, _ token:Token) -> (Bool))?) -> [String] {
+    public func parse(_ parse_until:((_ parser:TokenParser, _ token:Token) -> (Bool))?) -> [String]
+	{
         var nodes = [String]()
         
-        while tokens.count > 0 {
+        while tokens.count > 0
+		{
             let token = nextToken()!
             
-            switch token {
+            switch token
+			{
             case .text(let text):
                 nodes.append(text)
 
@@ -59,54 +64,33 @@ open class TokenParser {
         return nodes
     }
     
-    public func nextToken() -> Token? {
-        if tokens.count > 0 {
+    public func nextToken() -> Token?
+	{
+        if tokens.count > 0
+		{
             return tokens.remove(at: 0)
         }
         
         return nil
     }
     
-    private func compileFilter(_ token: String) -> Filter.Value {
-
-		func valueOrLiteral(for token: String) -> Filter.Value
-		{
-			let trimmedToken = token.trimmingWhitespaces
-
-			if trimmedToken.hasPrefix("\""), trimmedToken.hasSuffix("\"")
-			{
-				// This is a literal string. Strip its quotations.
-				return .string(trimmedToken.trim(character: "\""))
-			}
-			else if let integer = Int(trimmedToken)
-			{
-				// This is an integer literal (the integer constructor fails if a decimal point is found).
-				return .integer(integer)
-			}
-			else if let number = Decimal(string: trimmedToken)
-			{
-				// This is a decimal literal.
-				return .decimal(number)
-			}
-			else
-			{
-				// This is a variable name. Return its value, or an empty string.
-				return self.context.getValue(for: trimmedToken) ?? .nil
-			}
-		}
-
+    private func compileFilter(_ token: String) -> Filter.Value
+	{
 		let splitToken = token.split(separator: "|")
 
-		if splitToken.count == 1 {
+		if splitToken.count == 1
+		{
 			return valueOrLiteral(for: token)
 		}
 
 		var filteredValue = valueOrLiteral(for: String(splitToken.first!))
 
-		for filterString in splitToken[1...] {
-
+		for filterString in splitToken[1...]
+		{
 			let filterComponents = String(filterString).smartSplit(separator: ":")
-			guard filterComponents.count <= 2 else {
+
+			guard filterComponents.count <= 2 else
+			{
 				NSLog("Error: bad filter syntax: \(filterString). Stopping filter processing.")
 				return filteredValue
 			}
@@ -114,13 +98,17 @@ open class TokenParser {
 			let filterIdentifier = String(filterComponents.first!).trimmingWhitespaces
 			let filterParameters: [Filter.Value]?
 
-			if filterComponents.count == 1 {
+			if filterComponents.count == 1
+			{
 				filterParameters = []
-			} else {
+			}
+			else
+			{
 				filterParameters = String(filterComponents.last!).smartSplit(separator: ",").map({ valueOrLiteral(for: String($0)) })
 			}
 
-			guard let filter = filters.first(where: { $0.identifier == filterIdentifier }) else {
+			guard let filter = filters.first(where: { $0.identifier == filterIdentifier }) else
+			{
 				NSLog("Unknown filter name: \(filterIdentifier). Stopping filter processing.")
 				return filteredValue
 			}
@@ -130,4 +118,30 @@ open class TokenParser {
 
         return filteredValue
     }
+
+	private func valueOrLiteral(for token: String) -> Filter.Value
+	{
+		let trimmedToken = token.trimmingWhitespaces
+
+		if trimmedToken.hasPrefix("\""), trimmedToken.hasSuffix("\"")
+		{
+			// This is a literal string. Strip its quotations.
+			return .string(trimmedToken.trim(character: "\""))
+		}
+		else if let integer = Int(trimmedToken)
+		{
+			// This is an integer literal (the integer constructor fails if a decimal point is found).
+			return .integer(integer)
+		}
+		else if let number = Decimal(string: trimmedToken)
+		{
+			// This is a decimal literal.
+			return .decimal(number)
+		}
+		else
+		{
+			// This is a variable name. Return its value, or an empty string.
+			return self.context.getValue(for: trimmedToken) ?? .nil
+		}
+	}
 }
